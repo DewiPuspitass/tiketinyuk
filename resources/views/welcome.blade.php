e21858e9b0145dc4d6b1d3e4f9fbab6e8fe0c173
Sendiko <sarangtawon897@gmail.com>
2023-11-25 16:58:43 +0700

    enc(imp): put inside app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <button><a href="/login">Login Manager</a></button>
-    <button><a href="/loginStaff">Login Staff</a></button>
-</body>
-</html>
+@extends("layout.app")
+@section("main-container")
+<a href="/login" class="btn btn-secondary btn-lg">Login Manager</a>
+<div class="p-3"></div>
+<a href="/loginStaff" class="btn btn-primary btn-lg">Login Staff</a>
+@endsection
